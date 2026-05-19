6af3083d7bfc96904c980745c1b53c4e457b16de
Gaston Amurrio <elgaston011@gmail.com>
2026-05-19 15:08:03 -0300

    agrego config jwt

--- src/main/java/com/cale/demo/config/SecurityConfig.java
+++ src/main/java/com/cale/demo/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.cale.demo.config;
 
+import com.cale.demo.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll() // permite register/login
                         .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/cale/demo/security/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.cale.demo.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        System.out.println("AUTH HEADER: " + authHeader);
+
+        if  (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // es para que siga el flujo
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extraerEmail(token);
+
+        // SecurityContextHolder: “todavía NO hay usuario autenticado en este request”
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    email,
+                    null,
+                    new ArrayList<>()); // “este usuario ya está autenticado”
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // agrego ip, sesion, detalles http
+            SecurityContextHolder.getContext().setAuthentication(authToken); // “este request pertenece a este usuario"
+        }
+        filterChain.doFilter(request, response);
+    }
+}
