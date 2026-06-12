f49f0aed79fc24356d951ce474dcfdb60bef2e38
Gaston Amurrio <elgaston011@gmail.com>
2026-06-12 19:35:08 -0300

    agrego exepcion para token invalido y expirado

--- src/main/java/com/cale/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/cale/demo/security/JwtAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.cale.demo.security;
 
 import com.cale.demo.models.UsuarioModel;
 import com.cale.demo.repositories.UsuarioRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,12 +12,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -40,21 +42,45 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        String token = authHeader.substring(7);
-        String email = jwtService.extraerEmail(token);
-        UsuarioModel usuarioModel = usuarioRepository.findByEmail(email).orElse(null);
-        List<GrantedAuthority> grantedAuthorities = List.of( new SimpleGrantedAuthority(
-                "ROLE_" + usuarioModel.getRol().name()));
+        try {
+            String token = authHeader.substring(7);
+            String email = jwtService.extraerEmail(token);
+            UsuarioModel usuarioModel = usuarioRepository.findByEmail(email)
+                    .orElseThrow(() ->
+                    new UsernameNotFoundException("Usuario no encontrado"));
+            List<GrantedAuthority> grantedAuthorities = List.of( new SimpleGrantedAuthority(
+                    "ROLE_" + usuarioModel.getRol().name()));
 
-        // SecurityContextHolder: “todavía NO hay usuario autenticado en este request”
-        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    email,
-                    null,
-                    grantedAuthorities); // “este usuario ya está autenticado”
-            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // agrego ip, sesion, detalles http
-            SecurityContextHolder.getContext().setAuthentication(authToken); // “este request pertenece a este usuario"
+            // SecurityContextHolder: “todavía NO hay usuario autenticado en este request”
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        email,
+                        null,
+                        grantedAuthorities); // “este usuario ya está autenticado”
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // agrego ip, sesion, detalles http
+                SecurityContextHolder.getContext().setAuthentication(authToken); // “este request pertenece a este usuario"
+            }
+            filterChain.doFilter(request, response);
+        }catch (ExpiredJwtException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("""
+            {
+                "mensaje": "Token expirado"
+            }
+            """);
         }
-        filterChain.doFilter(request, response);
+        catch (JwtException e) {
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+
+            response.getWriter().write("""
+            {
+                "mensaje": "Token inválido"
+            }
+            """);
+
+        }
     }
 }
