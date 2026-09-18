6e7b11197b29430918ce15be285b3b1f16dc0ff0
Gaston Amurrio <elgaston011@gmail.com>
2026-09-18 19:32:49 -0300

    preparo datos para prod

--- src/main/java/com/cale/demo/config/DataInitializer.java
+++ src/main/java/com/cale/demo/config/DataInitializer.java
@@ -12,10 +12,12 @@ import com.cale.demo.repositories.UsuarioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 
+@Profile("!prod")
 @Configuration
 public class DataInitializer {
 
